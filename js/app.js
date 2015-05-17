2e4bd53ee7506d4bd4830fc974942a194529506d
isvictorious <me@isvictorious.com>
2015-05-17 15:00:38 -0400

    added hadouken animation

--- js/app.js
+++ js/app.js
@@ -6,5 +6,27 @@ $(document).ready(function(){
 	.mouseleave(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
+	})
+	.mousedown(function() {
+		// console.log('mousedown');
+		// play hadouken sound
+		$('.ryu-still').hide();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show().animate(
+			{'left': '1020px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '520px');
+			}
+		);
+		// animate hadouken across screen
+	})
+	.mouseup(function(){
+		// console.log('mouseup');
+		$('.ryu-ready').show();
+		$('.ryu-throwing').hide();
+		// $('.ryu-still').show();
 	});
 });
