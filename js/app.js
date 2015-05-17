ed22b6499eebf0139db0de368f98b23fa712ab44
isvictorious <me@isvictorious.com>
2015-05-17 15:09:30 -0400

    adding hadouken sound

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
+	function playHadouken () {
+	  $('#hadouken-sound')[0].volume = 0.5;
+	  $('#hadouken-sound')[0].load();
+	  $('#hadouken-sound')[0].play();
+	});
 	$('.ryu').mouseenter(function() {
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
